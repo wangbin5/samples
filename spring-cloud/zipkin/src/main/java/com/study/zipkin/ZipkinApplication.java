1bf0ac9d2dfcb69043df69d9071c16c1afb00327
141230 <141230@nd.com>
2017-12-12 21:48:21 +0800

    将日志存储到数据库

--- spring-cloud/zipkin/src/main/java/com/study/zipkin/ZipkinApplication.java
+++ spring-cloud/zipkin/src/main/java/com/study/zipkin/ZipkinApplication.java
@@ -1,7 +1,10 @@
 package com.study.zipkin;
 
+import com.github.kristofa.brave.Brave;
+import com.github.kristofa.brave.mysql.MySQLStatementInterceptorManagementBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
@@ -11,4 +14,9 @@ public class ZipkinApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZipkinApplication.class, args);
 	}
+	/*
+	@Bean
+	public MySQLStatementInterceptorManagementBean mySQLStatementInterceptorManagementBean(Brave brave) {
+		return new MySQLStatementInterceptorManagementBean(brave.clientTracer());
+	}*/
 }
